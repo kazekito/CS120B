32b5a1536955cfcf1b982f2a70194e44e68f6bd4
Bao Lam Le <blam025@wch136-24.cs.ucr.edu>
2021-04-10 13:46:09 -0700

    Completed part 1

--- Lab2_IntroToAVR/source/main.c
+++ Lab2_IntroToAVR/source/main.c
@@ -7,17 +7,32 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
+#endif	
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00; 
+	unsigned char tmpB = 0x00; 
+	unsigned char tmpA = 0x00; 
+while(1) {
+		
+		tmpA = PINA & 0x03;
+		
+		if (tmpA == 0x01) {
+			tmpB = (tmpB & 0xFC) | 0x01; 
+							 
+		}
+		 else {
+			tmpB = (tmpB & 0xFC) | 0x00; 
+							 
+		}	
 
-    /* Insert your solution below */
-    while (1) {
-
-    }
-    return 1;
+		PORTB = tmpB;	
+	}
+	return 0;
 }
+
